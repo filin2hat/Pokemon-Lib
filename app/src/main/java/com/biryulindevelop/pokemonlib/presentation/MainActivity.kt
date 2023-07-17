add17da65d621602d50adbec50d6efc499a2cb1b
Biryulin Dmitry <filin2hat@yandex.ru>
2023-07-17 13:58:08 +0300

    рефакторинг MainActivity

--- app/src/main/java/com/biryulindevelop/pokemonlib/presentation/MainActivity.kt
+++ app/src/main/java/com/biryulindevelop/pokemonlib/presentation/MainActivity.kt
@@ -3,12 +3,14 @@ package com.biryulindevelop.pokemonlib.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.*
 import androidx.navigation.navArgument
 import com.biryulindevelop.pokemonlib.R
 import com.biryulindevelop.pokemonlib.presentation.screens.details.PokemonDetailsScreen
@@ -23,33 +25,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             PokemonLibTheme {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = "pokemon_list_screen"
-                ) {
-                    composable("pokemon_list_screen") {
-                        PokemonListScreen(
-                            navController = navController
-                        )
-                    }
-                    composable(
-                        "pokemon_detail_screen/{dominantColor}/{pokemonName}",
-                        arguments = listOf(
-                            navArgument("dominantColor") {
-                                type = NavType.IntType
-
-                            },
-                            navArgument("pokemonName") {
-                                type = NavType.StringType
-                            }
-                        )
-                    ) {
+                NavHost(navController = navController, startDestination = Screen.PokemonList.route) {
+                    addScreen(Screen.PokemonList) { PokemonListScreen(navController = navController) }
+                    addScreen(Screen.PokemonDetails) { backStackEntry ->
                         val dominantColor = remember {
-                            val color = it.arguments?.getInt("dominantColor")
+                            val color = backStackEntry.arguments?.getInt("dominantColor")
                             color?.let { Color(it) } ?: Color.White
                         }
                         val pokemonName = remember {
-                            it.arguments?.getString("pokemonName") ?: getString(R.string.noname)
+                            backStackEntry.arguments?.getString("pokemonName") ?: getString(R.string.noname)
                         }
                         PokemonDetailsScreen(
                             dominantColor = dominantColor,
@@ -57,9 +41,28 @@ class MainActivity : ComponentActivity() {
                             navController = navController
                         )
                     }
-
                 }
             }
         }
     }
+
+    private fun NavGraphBuilder.addScreen(
+        screen: Screen,
+        content: @Composable (NavBackStackEntry) -> Unit
+    ) {
+        composable(screen.route, arguments = screen.arguments) { backStackEntry ->
+            content(backStackEntry)
+        }
+    }
+}
+
+sealed class Screen(val route: String, val arguments: List<NamedNavArgument>) {
+    object PokemonList : Screen("pokemon_list_screen", emptyList())
+    object PokemonDetails : Screen(
+        "pokemon_detail_screen/{dominantColor}/{pokemonName}",
+        listOf(
+            navArgument("dominantColor") { type = NavType.IntType },
+            navArgument("pokemonName") { type = NavType.StringType }
+        )
+    )
 }
